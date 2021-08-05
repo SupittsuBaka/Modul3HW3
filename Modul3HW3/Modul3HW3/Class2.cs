1f24fdbdbd37b75e46c61c53eebb90340daf76b6
Admin-PC\Admin <sashashmidt02@gmail.com>
2021-08-05 19:30:19 +0300

    Итоговый проект

---
+++ Modul3HW3/Modul3HW3/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW3
+{
+    public class Class2
+    {
+        private double _powResult;
+        public Func<int, bool> Calc(Func<int, int, int> powHandler, int x, int y)
+        {
+            _powResult = powHandler.Invoke(x, y);
+
+            return Result;
+        }
+
+        private bool Result(int item)
+        {
+            return _powResult % item == 0;
+        }
+    }
+}
--- Modul3HW3/Modul3HW3/Program.cs
+++ Modul3HW3/Modul3HW3/Program.cs
@@ -4,9 +4,18 @@ namespace Modul3HW3
 {
     public class Program
     {
+        public static void Show(bool result)
+        {
+            Console.WriteLine(result);
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var class1 = new Class1();
+            var class2 = new Class2();
+
+            class1.ShowHandler += Show;
+            class1.ShowHandler.Invoke(class2.Calc(class1.Pow, 3, 4).Invoke(2));
         }
     }
 }
